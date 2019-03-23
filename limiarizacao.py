d835a00b117bff528151d02604abeed5bfe7f75a
Victor <victor.outtes@gmail.com>
2019-03-23 14:46:22 -0300

    Primeiras aulas

--- limiarizacao.py
+++ limiarizacao.py
@@ -4,8 +4,9 @@ import cv2
 img = cv2.imread('images/lenna.png', cv2.IMREAD_COLOR)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-limiar = 50
+threshold_default = 125
 valor_max = 255
-th1, img_bw = cv2.threshold(img_gray, limiar, valor_max, cv2.THRESH_BINARY)
+th1, img_bw = cv2.threshold(img_gray, threshold_default, valor_max, cv2.THRESH_OTSU)
+print('Limiar = ' + str(th1))
 
 cv2.imwrite('images/limiarizacao/lenna_bw.png', img_bw)
---
+++ otsu.py
@@ -0,0 +1,64 @@
+import cv2
+
+
+def media_global(hist):
+    mg = 0
+    for i in range(len(hist)):
+        mg += i * hist[i]
+    return mg
+
+
+def media_classes(hist, limiar):
+    p1, p2 = prob_classes(hist, limiar)
+    m1 = 0
+    for i in range(0, limiar):
+        m1 += i * hist[i]
+
+    m2 = 0
+    for i in range(limiar, len(hist)):
+        m2 += i * hist[i]
+
+    med1 = 0
+    med2 = 0
+    if p1 > 0:
+        med1 = m1/p1
+    if p2 > 0:
+        med2 = m2/p2
+    return med1, med2
+
+
+def prob_classes(hist, limiar):
+    p1 = 0
+    for i in range(0, limiar):
+        p1 += hist[i]
+
+    p2 = 0
+    for i in range(limiar, len(hist)):
+        p2 += hist[i]
+    return p1, p2
+
+
+def var_max(p1, m1, p2, m2, mg):
+    return p1 * (m1 - mg)**2 + p2 * (m2 - mg)**2
+
+
+img = cv2.imread('images/moon.jpg', cv2.IMREAD_GRAYSCALE)
+qtd_pixels = img.size
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+hist_normalizado = [h[0]/qtd_pixels for h in hist]
+mg = media_global(hist_normalizado)
+
+limiar_best = 0
+variancia_best = 0
+
+for limiar in range(0, 256):
+    m1, m2 = media_classes(hist_normalizado, limiar)
+    p1, p2 = prob_classes(hist_normalizado, limiar)
+    variancia = var_max(p1, m1, p2, m2, mg)
+    if variancia > variancia_best:
+        variancia_best = variancia
+        limiar_best = limiar
+
+print('OTSU: ' + str(limiar_best))
+th1, img_bw = cv2.threshold(img, limiar_best, 255, cv2.THRESH_BINARY)
+cv2.imwrite('images/limiarizacao/otsu.png', img_bw)
